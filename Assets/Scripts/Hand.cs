0993e41dbdfc17ac90254440ef1a151342411b5f
LogansPc <loganhobson31@gmail.com>
2023-01-22 21:42:27 -0600

    Hands now disappear when grabbing an object.

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 public class Hand : MonoBehaviour
 {
     Animator animator;
+    SkinnedMeshRenderer mesh;
     private float triggerTarget;
     private float gripTarget;
     private float gripCurrent;
     private float triggerCurrent;
 
+    [Tooltip("Speed at which the fingers close. See speed of blend tree in animator as well.")]
     public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        mesh = GetComponentInChildren<SkinnedMeshRenderer>();
     }
 
     // Update is called once per frame
@@ -46,5 +49,10 @@ public class Hand : MonoBehaviour
             triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
             animator.SetFloat("Trigger", triggerCurrent);
         }
+    }
+
+    public void ToggleVisibility() //For when we grab things. So we can't see our hands and it doesn't intersect.
+    {
+        mesh.enabled = !mesh.enabled;
     }
 }
