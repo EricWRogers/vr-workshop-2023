7df9abcabe280b1d9780bf13e50960e06e4179af
Quinn Eakes <QMEakes1429@muleriders.saumag.edu>
2023-02-02 12:54:26 -0600

    New way of turning vignette on and off
    I have no idea if this works, but there's no errors so push away I guess

--- Assets/Scripts/HallucinationScripts/PostVignette.cs
+++ Assets/Scripts/HallucinationScripts/PostVignette.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
 
 public class PostVignette : HallucinationEvent
 {
-    public PostProcessVolume m_Volume;
-    Vignette m_Vignette;
-    
+    public GameObject vignetteObj;
+
     public override void PerformHallucinationEvent()
     {
         base.PerformHallucinationEvent();
-        m_Vignette.enabled.Override(false);
 
-        m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette);
+        if (vignetteObj == false)
+        {
+            vignetteObj.SetActive(true);
+        }
+        else if (vignetteObj == true)
+        {
+            vignetteObj.SetActive(false);
+        }
+        
     }
 }
