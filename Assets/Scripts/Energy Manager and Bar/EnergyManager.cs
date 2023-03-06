762435d6ea58d9e70f7f6302127c05b63d829347
Aiden Thacker <aidenthacker86@gmail.com>
2023-03-06 15:17:42 -0600

    Adding Water Bar into the game
    Works correctly for now

--- Assets/Scripts/Energy Manager and Bar/EnergyManager.cs
+++ Assets/Scripts/Energy Manager and Bar/EnergyManager.cs
@@ -7,11 +7,17 @@ public class EnergyManager : MonoBehaviour
 {
     public static EnergyManager instance;
     private CoffeeCup coffeeCup;
+    private BottleWater bottleWater;
     public UnityEvent gameOver;
 
+    public bool hasWater { get { return waterBar.slider.value > 0.0f; } }
     public string sceneName;
     public float energyLeft = 0;
+    public float hydrationLeft = 0;
+    public float waterLossRate = 0.01f;
+    public float energyLossRate = 0.01f;
     public EnergyBar energyBar;
+    public EnergyBar waterBar;
 
     void Awake()
     {
@@ -23,14 +29,22 @@ public class EnergyManager : MonoBehaviour
 
     void Start()
     {
-        coffeeCup = GameObject.FindObjectOfType<CoffeeCup>();
-        coffeeCup.onDrink.AddListener(AddPoints);
+        //coffeeCup = GameObject.FindObjectOfType<CoffeeCup>();
+        //coffeeCup.onDrink.AddListener(AddPoints);
+        bottleWater = GameObject.FindObjectOfType<BottleWater>();
+        bottleWater.onDrink.AddListener(AddHydration);
     }
 
     public void AddPoints(float amount)
     {
         energyLeft += amount;
         energyBar.slider.value = energyLeft;
+    }
+
+    public void AddHydration(float waterAmount)
+    {
+        hydrationLeft += waterAmount;
+        waterBar.slider.value =+ hydrationLeft;
     }
 
     public Tier GetTier()
@@ -42,6 +56,18 @@ public class EnergyManager : MonoBehaviour
         if (energyBar.slider.value <= .34 && energyBar.slider.value >= .00)
             return Tier.High;
         return Tier.Low;
+    }
+
+    public void TimeDown()
+    {
+        if(hasWater)
+        {
+           AddHydration(waterLossRate);
+        }
+        else
+        {
+            AddPoints(energyLossRate);
+        }
     }
 
     public void GameOver()
