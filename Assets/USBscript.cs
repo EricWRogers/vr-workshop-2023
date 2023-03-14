b3bdcf76ca23a600f3f1ae9d8013e12e8d9cbd9b
Will White <will.white.v1@gmail.com>
2023-03-14 10:51:13 -0700

    USB/TV interaction continued

--- Assets/USBscript.cs
+++ Assets/USBscript.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class USBscript : MonoBehaviour
 {
+    public GameObject tvScreen;
+
+    public Material[] slides;
+
+    public void slideshow()
+    {
+        for (Material in slides)
+        {
+            tvScreen.GetComponent(Material)
+        }
+    }
+
+    private void OnCollisionEnter(Collision tvScreen)
+    {
+        
+        
+
+    }
+
+    /*
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +35,27 @@ public class USBscript : MonoBehaviour
     {
         
     }
+    */
 }
+/*
+
+var change : boolean = true;
+
+function Start()
+{
+    changeTexture();
+}
+
+
+function changeTexture()
+{
+
+    while (change)
+    {
+        yield WaitForSeconds(0.5);
+renderer.material.mainTexture = texture1;
+yield WaitForSeconds(0.5);
+renderer.material.mainTexture = texture2;
+    }
+}
+*/
