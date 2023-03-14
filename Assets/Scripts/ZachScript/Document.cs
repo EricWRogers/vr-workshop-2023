0bd3042a2f2ed074bd1ea5681f316197bf5e26af
Kaylie Tipton <kaylietipton23@gmail.com>
2023-03-14 12:24:58 -0500

    Fix

--- Assets/Scripts/ZachScript/Document.cs
+++ Assets/Scripts/ZachScript/Document.cs
@@ -5,12 +5,12 @@ public class Document : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Pen_Sign"))
+        /*if (other.CompareTag("Pen_Sign"))
         {
             other.GetComponent<Pen>().Signature_Task();
 
             Debug.Log("Document Signed");
 
-        }
+        }*/
     }
 }
--- Assets/Scripts/ZachScript/Signature_Task.cs
+++ Assets/Scripts/ZachScript/Signature_Task.cs
@@ -6,15 +6,15 @@ public class Signature_Task : Task
     private Pen pen;
     private void Start()
     {
-        pen = FindObjectOfType<Pen>();
-        pen.SignEvent.AddListener(ManageTask);
+        //pen = FindObjectOfType<Pen>();
+        //pen.SignEvent.AddListener(ManageTask);
     }
     private void ManageTask()
     {
-        if (pen.Doc >= requiredAmount)
+        /*if (pen.Doc >= requiredAmount)
         {
             CompleteTask(this);
             Debug.Log("All done");
-        }
+        }*/
     }
 }
