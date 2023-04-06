4746210024cd43039a3a5446d04f56ca3a2eb8da
Ben <ben.lawrence0324@gmail.com>
2023-04-06 13:51:47 -0500

    Bug fixes on various tasks

--- Assets/Scripts/TaskScripts/StaplePaper/Stapler.cs
+++ Assets/Scripts/TaskScripts/StaplePaper/Stapler.cs
@@ -7,11 +7,17 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Stapler : MonoBehaviour
 {
     public Transform staplePoint;
+    public float stapleRadius;
     public GameObject staple;
     public float requiredDistance = .2f;
     private Staple_Task task;
     private List<Collider> colliders = new List<Collider>();
     private bool isFinished = true;
+    private Collider[] results;
+    private bool checkForPaper = false;
+    private int amountFound = 0;
+    private List<Collider> stapledPapers = new List<Collider>();
+    private int checkCount = 0;
 
 
     private void Start()
@@ -19,45 +25,144 @@ public class Stapler : MonoBehaviour
         task = FindObjectOfType<Staple_Task>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if (other.CompareTag("Paper"))
+        colliders.Clear();
+        if (checkForPaper)
         {
-            if (!colliders.Contains(other))
+            results = Physics.OverlapSphere(staplePoint.position, stapleRadius);
+
+            foreach (var result in results)
             {
-                colliders.Add(other);
+                if (stapledPapers.Contains(result))
+                {
+                    checkCount++;
+                }
             }
 
-            RaycastHit hit;
-            Physics.Raycast(staplePoint.position, Vector3.down, out hit, requiredDistance);
-
-            if (hit.transform != null)
+            if (checkCount == stapledPapers.Count && stapledPapers.Count > 0)
             {
-                if (hit.transform.CompareTag("Paper") && isFinished)
+                checkCount = 0;
+                return;
+            }
+
+            foreach (Collider result in results)
+            {
+                if (result.CompareTag("Paper"))
                 {
-                    isFinished = false;
-                    Collider newParent = colliders.Last();
-                    colliders.Remove(colliders.Last());
+                    amountFound++;
+                }
+            }
 
-                    foreach (Collider col in colliders)
+            if (amountFound >= 2)
+            {
+                foreach (Collider other in results)
+                {
+                    if (!colliders.Contains(other) && other.CompareTag("Paper"))
                     {
-                        HingeJoint hinge = col.gameObject.AddComponent<HingeJoint>();
-                        hinge.connectedBody = newParent.GetComponent<Rigidbody>();
-                        hinge.axis = new Vector3(0f, 1f, 0f);
-                        hinge.anchor = hit.transform.InverseTransformPoint(hit.point);
-
-                        //Instantiate(staple, hit.transform.InverseTransformPoint(hit.point), Quaternion.identity, newParent.transform);
-
-                        //col.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
-                        Destroy(col.gameObject.GetComponent<XRGrabInteractable>());
-                        //col.enabled = false;
-                        col.gameObject.layer = LayerMask.NameToLayer("StapledPaper");
-
-                        task.UpdateTask();
+                        colliders.Add(other);
                     }
-                    isFinished = true;
+                }
+
+                if (colliders.Count == 0)
+                    return;
+
+                RaycastHit hit;
+                Physics.Raycast(staplePoint.position, Vector3.down, out hit, requiredDistance);
+
+                if (hit.transform != null)
+                {
+                    if (hit.transform.CompareTag("Paper") && isFinished)
+                    {
+                        isFinished = false;
+                        Collider newParent = colliders.Last();
+                        colliders.Remove(colliders.Last());
+                        stapledPapers.Clear();
+
+                        foreach (Collider col in colliders)
+                        {
+                            HingeJoint hinge = col.transform.parent.gameObject.AddComponent<HingeJoint>();
+                            hinge.connectedBody = newParent.transform.parent.gameObject.GetComponent<Rigidbody>();
+                            hinge.axis = new Vector3(0f, 1f, 0f);
+                            hinge.anchor = hit.transform.InverseTransformPoint(hit.point);
+
+                            //Instantiate(staple, hit.transform.InverseTransformPoint(hit.point), Quaternion.identity, newParent.transform);
+                            
+                            foreach(Transform transform in col.transform.parent.GetComponentsInChildren<Transform>())
+                            {
+                                transform.gameObject.layer = LayerMask.NameToLayer("StapledPaper");
+                            }
+                            col.transform.parent.gameObject.name = "This One";
+
+                            stapledPapers.Add(col);
+
+                            task.UpdateTask();
+
+                        }
+                        colliders.Clear();
+                        isFinished = true;
+                    }
                 }
             }
         }
+    }
+
+    public void StartChecking()
+    {
+        checkForPaper = true;
+    }
+
+    public void StopChecking()
+    {
+        checkForPaper = false;
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Paper"))
+    //    {
+    //        if (!colliders.Contains(other))
+    //        {
+    //            colliders.Add(other);
+    //        }
+
+    //        RaycastHit hit;
+    //        Physics.Raycast(staplePoint.position, Vector3.down, out hit, requiredDistance);
+
+    //        if (hit.transform != null)
+    //        {
+    //            if (hit.transform.CompareTag("Paper") && isFinished)
+    //            {
+    //                isFinished = false;
+    //                Collider newParent = colliders.Last();
+    //                colliders.Remove(colliders.Last());
+
+    //                foreach (Collider col in colliders)
+    //                {
+    //                    HingeJoint hinge = col.gameObject.AddComponent<HingeJoint>();
+    //                    hinge.connectedBody = newParent.GetComponent<Rigidbody>();
+    //                    hinge.axis = new Vector3(0f, 1f, 0f);
+    //                    hinge.anchor = hit.transform.InverseTransformPoint(hit.point);
+
+    //                    //Instantiate(staple, hit.transform.InverseTransformPoint(hit.point), Quaternion.identity, newParent.transform);
+
+    //                    //col.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+    //                    Destroy(col.gameObject.GetComponent<XRGrabInteractable>());
+    //                    //col.enabled = false;
+    //                    col.gameObject.layer = LayerMask.NameToLayer("StapledPaper");
+
+    //                    task.UpdateTask();
+    //                    colliders.Clear();
+    //                }
+    //                isFinished = true;
+    //            }
+    //        }
+    //    }
+    //}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(staplePoint.position, stapleRadius);
     }
 }
