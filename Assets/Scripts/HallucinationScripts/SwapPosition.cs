f23ff9b3f80db37b0c81ef6b7b11a66c2afe3e5b
Kaylie Tipton <kaylietipton23@gmail.com>
2023-04-11 12:49:34 -0500

    Position Swap

---
+++ Assets/Scripts/HallucinationScripts/SwapPosition.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapPosition : HallucinationEvent
+{
+    public GameObject object1;
+    public GameObject object2;
+    private Vector3 tempPos;
+
+    public void Update()
+    {
+
+        tempPos = object1.transform.position;
+        object1.transform.position = object2.transform.position;
+        object2.transform.position = tempPos;
+    }
+
+
+    public override void PerformHallucinationEvent()
+    {
+        base.PerformHallucinationEvent();
+
+        tempPos = object1.transform.position;
+        object1.transform.position = object2.transform.position;
+        object2.transform.position = tempPos;
+
+        FinishHallucinationEvent();
+    }
+}
