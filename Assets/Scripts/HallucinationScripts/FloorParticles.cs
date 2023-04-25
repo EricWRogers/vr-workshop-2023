f8cc38862c4addd902616d24575a0387f8e55b53
Quinn Eakes <eakesq@yahoo.com>
2023-04-25 12:45:24 -0500

    Made Floor Particle Script
    Methinks it works

--- Assets/Scripts/HallucinationScripts/FloorParticles.cs
+++ Assets/Scripts/HallucinationScripts/FloorParticles.cs
@@ -2,15 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FloorParticles : HallucinationEvent
 {
-    public GameObject floorParts;
+    public ParticleSystem floor;
+
+    private void Start()
+    {
+        floor.Stop();
+    }
+
+    private bool isFloorOn = false;
+
     public override void PerformHallucinationEvent()
     {
         base.PerformHallucinationEvent();
 
-        floorParts.SetActive(true);
-
+        if (HallucinationEvent.isActive == true && isFloorOn == false)
+        {
+            TurnOnFloorParticles();
+            Debug.Log("turn on " + HallucinationEvent.isActive);
+        }
+        else if(HallucinationEvent.isActive == false && isFloorOn == true)
+        {
+            TurnOffFloorParticles();
+            Debug.Log("turn off " + HallucinationEvent.isActive);
+        }
+        else
+        {
+            Debug.Log("perhaps " + HallucinationEvent.isActive);
+        }
+        
         FinishHallucinationEvent();
+    }
+
+    public void TurnOnFloorParticles()
+    {
+        floor.Play();
+        isFloorOn = true;
+    }
+
+    public void TurnOffFloorParticles()
+    {
+        floor.Stop();
+        isFloorOn = false;
     }
 }
