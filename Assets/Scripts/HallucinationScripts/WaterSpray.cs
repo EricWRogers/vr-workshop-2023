898fb5d08d51fb1d342c204f5637a8b913258199
Kaylie Tipton <kaylietipton23@gmail.com>
2023-04-25 12:09:05 -0500

    The Miserable Broken Box Cast

--- Assets/Scripts/HallucinationScripts/WaterSpray.cs
+++ Assets/Scripts/HallucinationScripts/WaterSpray.cs
@@ -12,11 +12,43 @@ public class WaterSpray : HallucinationEvent
     public Timer waterTimer;
     public float waterTimeActive;
     public float maxDistance;
+    public float boxDistance;
+    public float boxSize;
 
+    private Collider m_Collider;
+
+    private bool m_hitDetect;
+    private RaycastHit m_hit;
     private bool isWaterActive = false;
 
+    
+    public void Start()
+    {
+        m_Collider = GetComponent<Collider>();
 
-    public void OnTriggerEnter(Collider other)
+    }
+
+    public void Update()
+    {   
+        m_hitDetect = Physics.BoxCast(m_Collider.bounds.center, transform.localScale * boxSize, transform.forward, out m_hit, transform.rotation, boxSize);
+        Debug.Log("Detect: " + m_hitDetect);
+        if(m_hitDetect)
+        {
+            Debug.Log("Hit: " + m_hit.collider.name);
+        }
+    }
+
+    public override void PerformHallucinationEvent()
+    {
+        base.PerformHallucinationEvent();
+        m_hitDetect = Physics.BoxCast(m_Collider.bounds.center, transform.localScale * boxSize, transform.forward, out m_hit, transform.rotation, boxDistance);
+        if(m_hitDetect)
+        {
+            Debug.Log("Hit: " + m_hit.collider.name);
+        }
+    }
+
+    /*public void OnTriggerEnter(Collider other)
     {
         PerformHallucinationEvent();
         if(other.tag == "Player")
@@ -28,6 +60,23 @@ public class WaterSpray : HallucinationEvent
                 waterTimer.StartTimer(waterTimeActive, waterTimer.AutoRestart);
             }
                 
+        }
+    }
+*/
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        if(m_hitDetect)
+        {
+            Gizmos.DrawRay(transform.position, transform.forward * m_hit.distance);
+            Gizmos.DrawWireCube(transform.position + transform.forward * boxDistance, transform.localScale * boxSize);
+        }
+        else
+        {
+            Gizmos.DrawRay(transform.position, transform.forward * boxDistance);
+            Gizmos.DrawWireCube(transform.position + transform.forward * boxDistance, transform.localScale * boxSize);
         }
     }
 
